e48c3860cafa3aaf1b517e8ee8857f3b50dfb40e
elisabeth_bakri <uiblee@gmail.com>
2018-11-02 13:53:21 -0400

    added more description for user input on city pick

--- bikeshare.py
+++ bikeshare.py
@@ -21,7 +21,7 @@ def get_filters():
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     while True:
         try:
-            city = input ("Which city's data would you like to look at?")
+            city = input ("Which city's data would you like to look at? (Chicago, New York City or Washington)")
             break
         except (ValueError, KeyboardInterrpt, TypeError):
             print("Please enter either Chicago, New York City or Washington")
